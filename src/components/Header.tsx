0190e019da0885e29db331bc021f066e9651337f
Pawan Ajjar K <1nc20ci027@ncetmail.com>
2024-10-20 00:20:39 +0530

    add home redirect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,12 +12,13 @@ export default function Header() {
         transition={{ delay: 0.2, type: 'spring', stiffness: 120 }}
         className="mb-4 sm:mb-0 flex items-center"
       >
-        <h1
-          className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500"
-        
-        >
-          FightMe
-        </h1>
+        <Link href="/">
+          <h1
+            className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-yellow-500 cursor-pointer"
+          >
+            FightMe
+          </h1>
+        </Link>
         <div className="ml-2 bg-red-600 text-white text-xs font-semibold px-2 py-1 rounded-full flex items-center">
           <div className="w-2 h-2 bg-white rounded-full mr-1 animate-pulse"></div>
           LIVE
